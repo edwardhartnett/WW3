4bf279fbd0e4278c5a96944040877e12106ac46b
Edward Hartnett <edward.hartnett@noaa.gov>
2024-04-12 17:54:53 -0600

    more unit tests

--- regtests/unittests/test_io2.F90
+++ regtests/unittests/test_io2.F90
@@ -99,13 +99,19 @@ program test_io
   print *, 'testing writing the WW3 binary point file in netCDF...'
 
   ! Write in netCDF.
+  ofiles(2) = 1
+  call w3iopon('WRITE', ndsop, iotest, imod)
+  if (iotest .ne. 0) stop 100
+  print *, 'OK!'
+  
+  ! Another timestep in netCDF.
   call w3iopon('WRITE', ndsop, iotest, imod)
   if (iotest .ne. 0) stop 100
   print *, 'OK!'
   
   print *, 'testing reading the WW3 binary point file in netCDF...'
   ipass2 = 0
-  call w3iopon('READ', ndsop, iotest, imod)
+  call w3iopon('READ', ndsop, iotest)
   print *, iotest
   if (iotest .ne. 0) stop 100
   print *, 'OK!'
